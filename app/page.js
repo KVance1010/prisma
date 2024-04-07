0e1778f16201c9c1c09f4f7d46c90812a48bf65b
KYLE VANCE <vanceofalifetime@protonmail.com>
2024-04-06 18:26:39 -0700

    added some content to the basic page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={}>
-    
+    <main className={styles.container}>
+      <h1>hello world</h1>
     </main>
   );
 }
